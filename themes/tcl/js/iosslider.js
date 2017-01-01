3444c9287cf0154dd2e3132710b652be2b47358e
vagrant <vagrant@tcl.dev>
2017-01-01 19:31:16 +0000

    js for Nav Menu at top of page

--- themes/tcl/js/iosslider.js
+++ themes/tcl/js/iosslider.js
@@ -13,6 +13,14 @@
         autoSlide: true,
         elasticPullResistance: 0.4
       });
+      $('nav#block-topnav h2.visually-hidden' ).click(function() {
+        if ($('nav#block-topnav ul.menu.nav').css('display') == 'none') {
+          $('nav#block-topnav ul.menu.nav').show(500);
+        }
+        else {
+          $('nav#block-topnav ul.menu.nav').hide(500);
+        }
+      });
     }
   };
 })(jQuery, Drupal, drupalSettings);
